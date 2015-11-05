a3402809429d1f39847641a98e70fc4b3bab8486
Jeisson Prieto <japrietov@unal.edu.c>
2015-11-05 16:49:42 -0500

    Clases de dominio agregadas.

---
+++ promosxpuntosapp/grails-app/domain/promosxpuntosapp/Campaign.groovy
@@ -0,0 +1,24 @@
+package CLASSES
+
+import org.grails.databinding.BindingFormat
+
+class Campaign{
+
+    String campaignName
+    String description
+    byte [] picture
+    Date dueDateCampaign
+    Date creationDateCampaign
+    int point
+
+
+    static constraints = {
+        campaignName(blank: false,unique: true,minSize: 5,maxSize: 30)
+        description(blank: false, unique: true, minSize: 30)
+        picture(maxSize: 3145728 /* 16K */)
+        point(blank:false, min: 1)
+        creationDateCampaign(nullable: true)
+        dueDateCampaign(blank: false)
+    }
+
+}
---
+++ promosxpuntosapp/grails-app/domain/promosxpuntosapp/Customer.groovy
@@ -0,0 +1,22 @@
+package CLASSES
+
+class Customer extends User{
+    String identification
+    String name
+    String contractNumber
+    Object logo
+    String description
+
+    static hasMany = [
+            rewards : Reward,
+            establishment : Establishment
+    ]
+
+    static constraints = {
+        identification(blank: false, unique: true)
+        name(size: 5..50)
+        contractNumber(size: 7..20)
+        description(maxSize: 500, nullable: true)
+        logo(nullable: true)
+    }
+}
